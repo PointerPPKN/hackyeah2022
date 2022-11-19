272c136d834f5426c70b8c9674c95b71b7d78b56
Jordi2233 <User123.Name456@protonmail.com>
2022-11-19 20:52:12 +0000

    1

--- backend/app.py
+++ backend/app.py
@@ -8,12 +8,9 @@ db = SQLAlchemy(app)
 
 class tag(db.Model):
     id = db.Column(db.Integer, db.Identity(start=0, cycle=True), primary_key=True)
-    tag_name = db.Column(db.String(50))
     tag_type = db.Column(db.Integer, nullable=False)
     cord_x = db.Column(db.Float, nullable=False)
     cord_y = db.Column(db.Float, nullable=False)
-
-
 
     def __repr__(self):
         return f'<tag> {self.tagname}'
