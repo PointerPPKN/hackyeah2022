5e0c7b8b92a05fa3e1d4326ffd5b9ecdcfe6c8b7
Geathriel <tomaszekjur@gmail.com>
2022-11-19 18:38:41 +0100

    modified:   backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,33 @@
+import json
 from flask import Flask, redirect, render_template, request
 from app import db
 from models import tag
-@app.route('/data/create' , methods = ['GET','POST'])
+
+
+@app.route('/data/create' , methods = ['POST'])
 def create():
-    if request.method == 'GET':
-        return render_template('createpage.html')
     if request.method == 'POST':
-        id = request.form['id']
         tag_name = request.form['tag_name']
         type = request.form['type']
         cord_x = request.form['cord_x']
         cord_y = request.form['cord_y']
-        localisations = tag(id=id, tag_name=tag_name, type=type, cord_x=cord_x, cord_y=cord_y)
+        localisations = tag(tag_name=tag_name, type=type, cord_x=cord_x, cord_y=cord_y)
         db.session.add(localisations)
         db.session.commit()
-        return redirect('/data')
+        return '', 204
+
+
 @app.route('/data')
 def ReadDataList():
-    ids = id.query.all()
-    return render_template('datalist.html',ids = ids)
+    tags = tag.query.all()
+    #sparsuj tags w jsona i zwróc jako json
+    return json.dumps(tags, indent=4), 200
+
+
 @app.route('/data/<int:id>')
 def ReadOneElement():
     localisation=tag.query.filter_by(id=id).first()
     if localisation:
-        return render_template('data.html', localisation = localisation)
-    return f"Localisation with id ={id} Doenst exist"
+        #zwróc localisation jako json
+        return json.dumps(localisation, indent=4), 200
+    return f"Localisation with id ={id} Doenst exist", 404
