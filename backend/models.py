ba4f1bab0cb52c0876680a168c0b564d989139c3
Jordi2233 <User123.Name456@protonmail.com>
2022-11-19 17:01:36 +0000

    changed tag column to tag_type

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from app import db
 class tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tag_name = db.Column(db.String(50))
-    type = db.Column(db.Integer, nullable=False)
+    tag_type = db.Column(db.Integer, nullable=False)
     cord_x = db.Column(db.Float, nullable=False)
     cord_y = db.Column(db.Float, nullable=False)
 
