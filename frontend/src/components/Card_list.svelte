db75c5c9575de26a0835f5ae3b7c5365103a53bb
alphatra <alpphatra@gmail.com>
2022-11-19 22:26:52 +0100

    Updejt

---
+++ frontend/src/components/Card_list.svelte
@@ -0,0 +1,33 @@
+<div class="container">
+    <div id="tittle">
+        <h1>Empik Wrocław</h1>
+        <p>Ul. Króla Janusza</p>
+    </div>
+    
+</div>
+
+<style>
+    .container{
+        position: relative;
+        width: 30vw;
+        margin-top: 5.5%;
+        margin-right: 3.5%;
+        height: 80vh; 
+        float: right;
+        box-sizing: border-box;
+        background: #608363;
+        border: 4px solid #000000;
+        box-shadow: 4px 4px 0px #000000;
+    }
+    .container > *{
+        text-align: left;
+    }
+    #tittle{
+        width: 60%;
+        background-color: red;
+    }
+    #tittle>h1,p{
+        margin-block-end: auto;
+        margin-block-start: auto;
+    }
+</style>
---
+++ frontend/src/components/Map.svelte
@@ -0,0 +1,56 @@
+<script>
+    import { onMount, onDestroy } from 'svelte'
+    import { Map, NavigationControl, Marker } from 'maplibre-gl';
+    import 'maplibre-gl/dist/maplibre-gl.css';
+  
+    let map;
+    let mapContainer;
+  
+    onMount(() => {
+  
+      const apiKey = 'ANx2EYNzbsX387SJBbPe';
+  
+      const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+  
+      map = new Map({
+        container: mapContainer,
+        style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${apiKey}`,
+        center: [initialState.lng, initialState.lat],
+        zoom: initialState.zoom
+      });
+      map.addControl(new NavigationControl(), 'top-right');
+      new Marker({color: "#FF0000"})
+        .setLngLat([139.7525,35.6846])
+        .addTo(map);
+    });
+  
+    onDestroy(() => {
+      map.remove();
+    });
+  </script>
+  
+  <div class="map-wrap">
+    <a href="https://www.maptiler.com" class="watermark"><img
+      src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo"/></a>
+    <div class="map" id="map" bind:this={mapContainer}></div>
+  </div>
+  
+  <style>
+  
+    .map-wrap {
+      width: 60vw;
+      float: left;
+      height: 100vh; /* calculate height of the screen minus the heading */
+    }
+  
+    .map {
+      height: 100%;
+    }
+  
+    .watermark {
+      position: absolute;
+      left: 10px;
+      bottom: 10px;
+      z-index: 999;
+    }
+  </style>
