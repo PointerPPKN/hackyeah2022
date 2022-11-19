0642d436d5bbc51748f8ff1a68672bd4b1497807
Geathriel <tomaszekjur@gmail.com>
2022-11-19 18:57:16 +0100

    modified:   backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -30,4 +30,23 @@ def ReadOneElement():
     if localisation:
         #zwróc localisation jako json
         return json.dumps(localisation, indent=4), 200
-    return f"Localisation with id ={id} Doenst exist", 404
+    return f"Localisation with id ={id} Does not exist", 404
+
+@app.route('/data/<int:id>/update',methods = ['POST'])
+def update(id):
+    localisations = tag.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        if localisations:
+            db.session.delete(localisations)
+            db.session.commit()
+ 
+            tag_name = request.form['tag_name']
+            type = request.form['type']
+            cord_x = request.form['cord_x']
+            cord_y = request.form['cord_y']
+            localisations = tag(tag_name=tag_name, type=type, cord_x=cord_x, cord_y=cord_y)
+ 
+            db.session.add(localisations)
+            db.session.commit()
+            return '', 204
+        return f"localisation with id = {id} Does not exist", 404
