981ecabba876df4490f4c4d84490a50a67723ba0
Jordi2233 <User123.Name456@protonmail.com>
2022-11-19 16:41:37 +0000

    type, x_cord, y_cord added to db structure

--- backend/models.py
+++ backend/models.py
@@ -2,8 +2,13 @@ from app import db
 
 class tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tag_name = db.Column(db.String(50), nullable=False)
-    
+    tag_name = db.Column(db.String(50))
+    type = db.Column(db.Integer, nullable=False)
+    cord_x = db.Column(db.Float, nullable=False)
+    cord_y = db.Column(db.Float, nullable=False)
+
+
+
     def __repr__(self):
         return f'<tag> {self.tagname}'
         
