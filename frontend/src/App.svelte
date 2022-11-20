6a2dae3664d02c3eb6937ed69c9498e0f0a15b1e
alphatra <alpphatra@gmail.com>
2022-11-20 01:06:01 +0100

    Updejt🥸

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,14 +1,29 @@
 <script>
+  import { Router, Link, Route } from "svelte-routing";
   import Map from './components/Map.svelte';
   import Card_place from './components/Card_place.svelte';
   import Card_list from './components/Card_list.svelte';
+  import Card_generator from './components/Card_generator.svelte';
+  export let url = "";
 </script>
 
-<div class="app">
-  <Map x={"51.107883"} y={"17.038538"}/>
-  <Card_place />
-  <Card_list />
-</div>
+<Router url="{url}">
+    <Route path="about">
+      <div class="app">
+        <Map x={"56.107883"} y={"27.038538"}/>
+        <Card_place />
+        <Card_list />
+      </div>
+    </Route>
+
+    <Route path="/">
+      <div class="app">
+        <Map x={"51.107883"} y={"17.038538"}/>
+        <Card_place />
+        <Card_generator />
+      </div>
+    </Route>
+</Router>
 
 <style>
   .app {
